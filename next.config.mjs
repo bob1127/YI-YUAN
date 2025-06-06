e44bfc77551e5808803d794adb81745b18adcdd2
bob1127 <61307295+bob1127@users.noreply.github.com>
2025-06-06 14:03:45 +0800

    sdfd

--- next.config.mjs
+++ next.config.mjs
@@ -7,23 +7,7 @@ const nextConfig = {
         hostname: "**", // 允許任何圖片來源
       },
     ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.yiyuan-arch.com.tw' }],
-        destination: 'https://yiyuan-arch.com.tw/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'yi-yuan.vercel.app' }],
-        destination: 'https://yiyuan-arch.com.tw/:path*',
-        permanent: true,
-      },
-    ];
-  },
+  }
 };
 
 export default nextConfig;
